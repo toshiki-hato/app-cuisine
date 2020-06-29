d99acffc7ad488750aa170bc28ced1952d6ec1af
toshiki-hato <66351093+toshiki-hato@users.noreply.github.com>
2020-06-29 16:37:50 +0900

    トップビュー　完成(仮)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('bootstrap/dist/js/bootstrap.min.js')
+// = require bootstrap/dist/css/bootstrap.min.css
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +17,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap';
+import '../stylesheets/application';
+import '@fortawesome/fontawesome-free/js/all'
+require("bootstrap");
+require("@fortawesome/fontawesome-free");
