9013dd31a1b37d6f53149a8d53c9c46c9fd9f0a1
toshiki-hato <66351093+toshiki-hato@users.noreply.github.com>
2020-09-03 16:39:07 +0900

    ｖ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def default_url
+    'board_placeholder.png'
+  end
 end
